38749c12eaa959f228f9fc21244a02512033db43
Stanislav Senotrusov <stan@senotrusov.com>
2011-07-09 19:55:12 +0900

    ActiveRecord::Base loading

--- lib/workety/simple_thread.rb
+++ lib/workety/simple_thread.rb
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-class Workety::TestThread
+class SimpleWorketyThread
   # Before dropping privileges 
   def initialize
   end
