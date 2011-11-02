0a6e741e4c2e1fb5583e7ef12cc98cba5465eeae
Stanislav Senotrusov <stan@senotrusov.com>
2011-11-02 17:03:48 +0800

    I just hope Rails timezone is thread-safe for now

--- lib/workety.rb
+++ lib/workety.rb
@@ -23,7 +23,6 @@ require 'workety/extensions/process.rb'
 require 'workety/extensions/signal.rb'
 require 'workety/extensions/socket.rb'
 require 'workety/extensions/thread.rb'
-require 'workety/extensions/tzinfo.rb' if Gem.available? 'tzinfo'
 
 
 require 'workety/timed_exit.rb'
